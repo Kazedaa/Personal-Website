977767bd044f586afd8426c4e125be9af5567319
Hemang J Jamadagni <120291477+Kazedaa@users.noreply.github.com>
2024-08-28 16:39:06 +0530

    removed publications

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Publications", link: "#publications" },
+  // { name: "Publications", link: "#publications" },
   { name: "Experience", link: "#experience" },
   { name: "Contact", link: "#contact" },
 ];
